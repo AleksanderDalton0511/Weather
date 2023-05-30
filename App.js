1ada9c104b5e312bc33173582f76f10445348475
Aleksander Dalton <Aleksander.Dalton@tptlive.ee>
2023-05-30 17:25:33 +0300

    Input field

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: 'lawngreen'}}>CHOOSE YOUR CITY:</Text>
+      <TextInput style={{backgroundColor: 'darkgrey', width: "40%", marginTop: "3%"}}></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
