7e89e3dfce7c8a6dae9f3cf2fa6406e58d2a325e
Aleksander Dalton <Aleksander.Dalton@tptlive.ee>
2023-05-31 15:49:32 +0300

    API

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{color: 'lawngreen'}}>CHOOSE YOUR CITY:</Text>
-      <TextInput style={{backgroundColor: 'darkgrey', width: "40%", marginTop: "3%"}}></TextInput>
+      <TextInput value={city} style={{backgroundColor: 'darkgrey', width: "40%", marginTop: "3%"}}></TextInput>
+      <Text style={{color: 'lawngreen'}}>result</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +20,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+let city = "Tallinn";
+
+async function request(){
+  const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=' + city + '&days=3&dt=2023-05-31';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3c9510ca70mshf8fe7463f988101p197cb5jsn5804d7f8fa69',
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+}
+
+request();
