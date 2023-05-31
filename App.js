4ec36f090109df2cc4b1c4222c049df43a868da4
Aleksander Dalton <Aleksander.Dalton@tptlive.ee>
2023-05-31 16:56:40 +0300

    Render results

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+  const[results, setResults] = useState();
+  let city = "Tallinn";
+
+const request = async () => {const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=' + city + '&days=3&dt=2023-05-31';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3c9510ca70mshf8fe7463f988101p197cb5jsn5804d7f8fa69',
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+  setResults(result);
+} catch (error) {
+	console.error(error);
+}
+}
+request();
+
   return (
     <View style={styles.container}>
       <Text style={{color: 'lawngreen'}}>CHOOSE YOUR CITY:</Text>
       <TextInput value={city} style={{backgroundColor: 'darkgrey', width: "40%", marginTop: "3%"}}></TextInput>
-      <Text style={{color: 'lawngreen'}}>result</Text>
+      <Text style={{color: 'lawngreen'}}>{results}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,26 +44,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-let city = "Tallinn";
-
-async function request(){
-  const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=' + city + '&days=3&dt=2023-05-31';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '3c9510ca70mshf8fe7463f988101p197cb5jsn5804d7f8fa69',
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-}
-
-request();
