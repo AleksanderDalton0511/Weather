9695fc2e868bcf196342ed3f1cdd607a55b50f13
Aleksander Dalton <Aleksander.Dalton@tptlive.ee>
2023-06-01 12:07:45 +0300

    Date

--- App.js
+++ App.js
@@ -6,7 +6,22 @@ export default function App() {
   const[results, setResults] = useState();
   let city = "Tallinn";
 
-const request = async () => {const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=' + city + '&days=3&dt=2023-05-31';
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = mm+'-'+dd+'-'+yyyy;
+console.log(today);
+
+const request = async () => {const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=' + city + '&days=3&dt=' + today;
 const options = {
 	method: 'GET',
 	headers: {
