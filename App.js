73da53509020ceb6e0a3b427d8b157966d6ddd02
AleksanderDalton0511 <Aleksander.DAlton@tptlive.ee>
2023-08-29 09:59:29 +0300

    default statusbar

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { BackHandler } from "react-native";
+import { StatusBar } from 'expo-status-bar';
 import * as Location from 'expo-location';
 import { StyleSheet, Text, TextInput, View, TouchableOpacity, PermissionsAndroid, Image } from 'react-native';
 import React, { useState, useEffect } from 'react';
@@ -225,7 +226,7 @@ try {
       <TouchableOpacity onPress={(prevCheck) => setDaily(prevCheck => !prevCheck)} style={{backgroundColor: "darkgrey", paddingRight: "5%", paddingLeft: "5%", paddingTop: "1%", paddingBottom: "1%", marginTop: `${ margin }`}}><Text style={{color: "red"}}>{word}</Text></TouchableOpacity>
 
     {data}
-
+    <StatusBar style="auto" />
       </View>
     )}
     catch (error) {
@@ -235,6 +236,7 @@ try {
           <TextInput onSubmitEditing = {() => show()} onChangeText={newText => setCity(newText)} style={{backgroundColor: 'darkgrey', width: "40%", marginTop: "3%", textAlign:"center", color: "red"}}></TextInput>
           <Text style={{color: "red"}}>Such city does not exist!</Text>
           <TouchableOpacity style={{marginTop: "2%", backgroundColor: "#7B858D"}} onPress={MyLocation}><Text style={{margin: "2%", color: "red"}}>MY POSITION</Text></TouchableOpacity>
+          <StatusBar style="auto" />
         </View>
       );
     }
@@ -245,6 +247,7 @@ try {
         <Text style={{color: 'white'}}>CHOOSE YOUR CITY:</Text>
         <TextInput onSubmitEditing = {() => show()} onChangeText={newText => setCity(newText)} style={{backgroundColor: 'darkgrey', width: "40%", marginTop: "3%", textAlign:"center", color: "red"}}></TextInput>
         <TouchableOpacity style={{marginTop: "6%", backgroundColor: "#7B858D"}} onPress={MyLocation}><Text style={{margin: "2%", color: "red"}}>MY POSITION</Text></TouchableOpacity>
+        <StatusBar style="auto" />
       </View>
     );
   }
